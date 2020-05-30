304895421d8f9c69d9f7426e35b138f5b3f864ae
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-05-30 14:20:16 +0000

    Utilisation arch pour docker-run.sh

--- nginx/test/docker-run.sh
+++ nginx/test/docker-run.sh
@@ -6,22 +6,11 @@ CONF_FOLDER=`pwd`/../res/conf.d
 MODS_FOLDER=`pwd`/modules
 
 # Override version (e.g. pour utiliser x86_64_...)
-VERSION=x86_64_1.26_0
+ARCH=`uname -m`
+VERSION=${ARCH}_${VERSION}
 IMAGE_DOCKER=$REPO/$NAME:$VERSION
 
 echo Image docker : $IMAGE_DOCKER
-
-# docker run -d --rm \
-#   -p 80:80 \
-#   -p 443:443 \
-#   $REPO/$NAME:$VERSION
-
-# docker run --rm -it \
-#   -p 80:80 \
-#   -p 443:443 \
-#   -v $CONF_FOLDER:/etc/nginx/conf.d \
-#   -v /home/mathieu/mgdev/certs:/certs \
-#   $REPO/$NAME:$VERSION
 
 docker run --rm -it \
   --network host \
