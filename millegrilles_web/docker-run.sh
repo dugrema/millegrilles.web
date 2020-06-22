303e6677ba5f21ac2ddba0c3895d16c1e52c4a4b
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-06-22 00:51:36 +0000

    Build 1.28.2 web protege

--- millegrilles_web/docker-run.sh
+++ millegrilles_web/docker-run.sh
@@ -3,16 +3,16 @@
 source image_info.txt
 
 # Override version (e.g. pour utiliser x86_64_...)
-VERSION=x86_64_1.28.1
+VERSION=x86_64_1.28.2
 IMAGE_DOCKER=$REPO/$NAME:$VERSION
 
 echo Image docker : $IMAGE_DOCKER
 
 export HOST=`hostname`
 CERT_FOLDER=/home/mathieu/mgdev/certs
-export MG_MQ_CAFILE=/certs/2DE28fU9miAD.pki.millegrille.cert.20200531123203
-export MG_MQ_CERTFILE=/certs/2DE28fU9miAD.pki.coupdoeil.cert.20200531123213
-export MG_MQ_KEYFILE=/certs/2DE28fU9miAD.pki.coupdoeil.key.20200531123213
+export MG_MQ_CAFILE=/certs/pki.millegrille.cert
+export MG_MQ_CERTFILE=/certs/pki.web_protege.cert
+export MG_MQ_KEYFILE=/certs/pki.web_protege.key
 export MG_MQ_URL=amqps://$HOST:5673
 export PORT=3000
 
