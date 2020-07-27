751d456230f59e7d7bf913164481351553aa3aad
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-07-27 01:08:45 +0000

    Ajout client pour build apps protegees

---
+++ millegrilles_web/client/src/mappingDependances.js
@@ -0,0 +1,10 @@
+import Coupdoeil from 'deps/coupdoeil'
+
+export const MAPPING_DEPENDANCES = [
+  {
+    nom: "coupdoeil",
+    nomFormatte: "Coup D'Oeil",
+    dom: Coupdoeil,
+    securite: "3.protege"
+  }
+]
