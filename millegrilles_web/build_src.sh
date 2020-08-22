7e10e51add6ffbceb581b18c16c3ef2f60845b64
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-08-22 19:47:24 +0000

    Ajout build path pour download fichier

--- millegrilles_web/build_src.sh
+++ millegrilles_web/build_src.sh
@@ -103,6 +103,7 @@ makeManifest() {
 REP_COURANT=`pwd`
 REP_STATIC_GLOBAL=${REP_COURANT}/static
 BUILD_FILE="${NAME}.${VERSION}.tar.gz"
+BUILD_PATH="git/millegrilles.web/millegrilles_web"
 
 docker pull node:12
 npm install --production
