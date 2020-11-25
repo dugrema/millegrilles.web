71198575be8f4fe71aafd5bd591e72b86f6ae2bb
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-11-25 23:15:13 +0000

    Ajout manifeset

--- millegrilles_web/client/src/mappingDependances.js
+++ millegrilles_web/client/src/mappingDependances.js
@@ -1,28 +1,8 @@
-// import React from 'react'
-//
-// import TestApp2 from './containers/Test'
-//
-// class TestApp extends React.Component {
-//   render() {
-//     return <p>Test app protegee simple</p>
-//   }
-// }
-//
-// export const MAPPING_DEPENDANCES = [
-//   {
-//     nom: "testapp",
-//     nomFormatte: "Application de test",
-//     dom: TestApp,
-//     securite: "3.protege"
-//   },
-//   {
-//     nom: "testapp2",
-//     nomFormatte: "Application de test #2",
-//     // Exemple de code splitting
-//     load: async _=>{return (await import('./containers/Test')).default},
-//     securite: "3.protege"
-//   }
-// ]
+import manifest from './manifest.build.js'
+
+export function getManifest() {
+  return manifest
+}
 
 export const MAPPING_DEPENDANCES = [
   {
